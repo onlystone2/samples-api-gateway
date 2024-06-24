9adb151d81c8b390b3f7d149a2e8bb8583a33565
onlystone <onlystone@gmail.com>
2024-06-24 10:03:57 +0900

    refactor package name

--- src/main/java/com/kubeworks/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/kubeworks/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.kaankaplan.apigateway;
+package com.kubeworks.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
